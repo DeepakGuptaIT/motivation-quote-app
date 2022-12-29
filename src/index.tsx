58b797cdaaa22d75052c4dc7d1d8ad2f3f973a7f
DeepakGuptaIT <deepakguptaoptimistic@gmail.com>
2022-12-29 16:01:56 +0000

    firebase init setup

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,30 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
+// add firebase setup
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDJsAjDpjd7hrw0ywukT2N4FdlsCEgPmPE",
+  authDomain: "motivation-quote-e5fe0.firebaseapp.com",
+  projectId: "motivation-quote-e5fe0",
+  storageBucket: "motivation-quote-e5fe0.appspot.com",
+  messagingSenderId: "675836629919",
+  appId: "1:675836629919:web:6e982b970c699dcb23a3aa",
+  measurementId: "G-7Z648GTL10"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
 const container = document.getElementById('root');
 const root = createRoot(container!);
 root.render(
